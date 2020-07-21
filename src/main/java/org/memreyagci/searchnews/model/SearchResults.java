4f0620270e8a02129eee70e89596b2958434dc74
memreyagci <meydeveloper@tutanota.com>
2020-07-21 18:11:51 +0200

    SearchResults model added. It defines the data of the search results to be shown in ResultsPanel.

---
+++ src/main/java/org/memreyagci/searchnews/model/SearchResults.java
@@ -0,0 +1,51 @@
+package org.memreyagci.searchnews.model;
+
+/*
+A model for defining the data of the search results to be shown in ResultsPanel.
+ */
+
+public class SearchResults {
+    private String title;
+    private String source;
+    private String date;
+    private String url;
+
+    public SearchResults(String title, String source, String date, String url) {
+        this.title = title;
+        this.source = source;
+        this.date = date;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
