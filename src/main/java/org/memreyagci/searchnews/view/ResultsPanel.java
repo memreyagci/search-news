3e64a69be65db89df41ea1eff7dc72a2496d0ca9
memreyagci <meydeveloper@tutanota.com>
2020-07-21 19:37:22 +0200

    ResultsPanel components are created, and ListCellRenderer is implemented.

--- src/main/java/org/memreyagci/searchnews/view/ResultsPanel.java
+++ src/main/java/org/memreyagci/searchnews/view/ResultsPanel.java
@@ -1,9 +1,32 @@
 package org.memreyagci.searchnews.view;
 
+import org.memreyagci.searchnews.model.SearchResults;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class ResultsPanel extends JPanel {
-    public ResultsPanel() throws HeadlessException {
+// Result renderer.
+
+public class ResultsPanel extends JPanel implements ListCellRenderer<SearchResults> {
+
+    private JLabel title = new JLabel();
+    private JLabel source = new JLabel();
+    private JLabel date = new JLabel();
+
+    public ResultsPanel() {
+
+        JPanel newsPanel = new JPanel();
+        newsPanel.add(title);
+        newsPanel.add(source);
+        newsPanel.add(date);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends SearchResults> list, SearchResults searchResults, int index, boolean isSelected, boolean cellHasFocus) {
+        title.setText(searchResults.getTitle());
+        source.setText(searchResults.getSource());
+        date.setText(searchResults.getSource());
+
+        return this;
     }
 }
