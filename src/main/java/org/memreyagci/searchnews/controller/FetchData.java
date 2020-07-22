e62e0b2b3e77bed96880ed21788ad6213b751096
memreyagci <meydeveloper@tutanota.com>
2020-07-23 00:38:14 +0200

    FetchData.fetchNews now return the JSONObject that is got from NewsAPI.

--- src/main/java/org/memreyagci/searchnews/controller/FetchData.java
+++ src/main/java/org/memreyagci/searchnews/controller/FetchData.java
@@ -3,13 +3,14 @@ package org.memreyagci.searchnews.controller;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 
 public class FetchData {
 
-    public void fetchNews(String url){
+    public JSONObject fetchNews(String url){
         HttpResponse<JsonNode> httpResponse = Unirest.get(url)
                 .asJson();
 
-        String result = httpResponse.getBody().toPrettyString();
+        return httpResponse.getBody().getObject();
     }
 }
