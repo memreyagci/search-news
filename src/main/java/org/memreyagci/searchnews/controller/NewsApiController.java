d6e41ceb232060d3ec6aae2dbfe7d2b10c7e12b4
memreyagci <meydeveloper@tutanota.com>
2020-07-23 12:17:38 +0200

    New NewsApiControler parameters are created.

--- src/main/java/org/memreyagci/searchnews/controller/NewsApiController.java
+++ src/main/java/org/memreyagci/searchnews/controller/NewsApiController.java
@@ -1,6 +1,8 @@
 package org.memreyagci.searchnews.controller;
 
+import kong.unirest.json.JSONArray;
 import org.memreyagci.searchnews.model.NewsApi;
+import org.memreyagci.searchnews.model.SearchResults;
 import org.memreyagci.searchnews.view.ResultsPanel;
 import org.memreyagci.searchnews.view.SearchPanel;
 
@@ -9,14 +11,22 @@ import java.util.Date;
 
 public class NewsApiController {
     private SearchPanel searchPanel;
+    private ResultsPanel resultsPanel;
+
     private SearchResultsController searchResultsController;
+
     private NewsApi newsApi;
+    private SearchResults searchResults;
+
     private FetchData fetchData = new FetchData();
     private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-    public NewsApiController(SearchPanel searchPanel, NewsApi newsApi) {
+    public NewsApiController(SearchPanel searchPanel, ResultsPanel resultsPanel, NewsApi newsApi, SearchResults searchResults, SearchResultsController searchResultsController) {
         this.searchPanel = searchPanel;
+        this.resultsPanel = resultsPanel;
         this.newsApi = newsApi;
+        this.searchResults = searchResults;
+        this.searchResultsController = searchResultsController;
     }
 
     public void initController() {
@@ -26,6 +36,8 @@ public class NewsApiController {
 
             // Setting the fetched JSONObject to SearchResultController.results
             searchResultsController.setResults(fetchData.fetchNews(generateUrl.getUrl(newsApi)));
+
+            createRendererModel();
         });
     }
 
@@ -39,5 +51,17 @@ public class NewsApiController {
         newsApi.setProvidedDateFrom(simpleDateFormat.format(selectedFromDate));
         newsApi.setProvidedDateTo(simpleDateFormat.format(selectedToDate));
         newsApi.setProvidedSortBy(searchPanel.getSelectedRadioButtonText(searchPanel.getRadioButtonGroup()));
+    }
+
+    private void createRendererModel() {
+        JSONArray articles = searchResultsController.getResults().getJSONArray("articles");
+
+        for(int i=0; i<articles.length(); i++) {
+            searchResults.setTitle(articles.getJSONObject(i).getString("title"));
+            searchResults.setSource(articles.getJSONObject(i).getJSONObject("source").getString("name"));
+            searchResults.setDate(articles.getJSONObject(i).getString("publishedAt"));
+
+            resultsPanel.getDefaultListModel().addElement(searchResults);
+        }
     }
 }
