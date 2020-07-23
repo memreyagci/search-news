123eadbe6d3b6de0c210d1e93b39fef042a52701
memreyagci <meydeveloper@tutanota.com>
2020-07-23 12:21:37 +0200

    A SearchResults object is is initiated and a getter for results JSONObject is created.

--- src/main/java/org/memreyagci/searchnews/controller/SearchResultsController.java
+++ src/main/java/org/memreyagci/searchnews/controller/SearchResultsController.java
@@ -2,13 +2,19 @@ package org.memreyagci.searchnews.controller;
 
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
+import org.memreyagci.searchnews.model.SearchResults;
 
 public class SearchResultsController {
+    private SearchResults searchResults;
 
     private JSONObject results;
     private JSONArray articles;
 
     public void setResults(JSONObject results) {
         this.results = results;
+    }
+
+    public JSONObject getResults() {
+        return results;
     }
 }
